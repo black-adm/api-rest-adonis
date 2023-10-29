e4fd17d2db694e3fe2065e5a492553a9df5d403e
black-adm <mattheuscontato17@gmail.com>
2023-10-28 22:40:35 -0300

    list, create and delete user methods

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,13 +1,35 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import User from "App/Models/User";
+import { randomUUID } from "crypto";
 
 export default class UsersController {
-  public async index({}: HttpContextContract) {}
+  public async index({}: HttpContextContract) {
+    const users = await User.all();
+    return users;
+  }
 
-  public async store({}: HttpContextContract) {}
+  public async store({ request }: HttpContextContract) {
+    const data = request.only(["name", "email", "password"]);
+
+    const user = new User();
+    user.id = randomUUID();
+    user.fill(data);
+
+    await user.save();
+    console.log(user.$isPersisted);
+    return user;
+  }
 
   public async show({}: HttpContextContract) {}
 
   public async update({}: HttpContextContract) {}
 
-  public async destroy({}: HttpContextContract) {}
+  public async destroy({ request }: HttpContextContract) {
+    const data = request.param("id");
+
+    const user = await User.findOrFail(data);
+    await user.delete();
+
+    return user;
+  }
 }

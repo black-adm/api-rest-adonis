0326dc39f1943534c6fd0224eff60f3024eb23d9
black-adm <mattheuscontato17@gmail.com>
2023-10-29 06:06:05 -0300

    Add route login

--- start/routes.ts
+++ start/routes.ts
@@ -1,7 +1,14 @@
 import Route from "@ioc:Adonis/Core/Route";
 
 Route.get("/", async () => {
-  return { hello: "world" };
+  return { message: true };
+});
+
+Route.post("/login", async ({ auth, request }) => {
+  const email = request.input("email");
+  const password = request.input("password");
+
+  await auth.use("api").attempt(email, password);
 });
 
 Route.resource("/users", "UsersController");
